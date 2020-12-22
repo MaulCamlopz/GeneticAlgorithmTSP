450c53fbe2ec797b86f0d6a1042ba51c97888c4e
Maul Camlopz <b22braul@live.com>
2020-12-22 00:16:11 -0600

    Update GeneticAlgorithm

---
+++ GeneticAlgorithm.py
@@ -0,0 +1,8 @@
+#
+#  GeneticAlgorithm.py
+#  Tarea XI: Aplicación de los Algoritmos Genéticos en problemas de búsqueda NP
+#
+#  Created by Camacho López Raúl Josafath.
+#  Created by García Alonso Giovanni.
+#  Copyright © 22/12/2020 UACM. All rights reserved.
+#
