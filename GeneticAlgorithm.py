70c838134da078efc5e354d514a4471178540a1d
Maul Camlopz <b22braul@live.com>
2020-12-22 01:31:00 -0600

    add function createMatrix

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -6,3 +6,16 @@
 #  Created by García Alonso Giovanni.
 #  Copyright © 22/12/2020 UACM. All rights reserved.
 #
+import math
+import random
+
+def createMatrix(rows, columns):
+    matrix = []
+    poblacion = []
+    for i in range(rows):
+        column = []
+        for j in range(columns):
+            column.append(random.randint(10, 50))
+        matrix.append(column)
+    return(matrix)
+
