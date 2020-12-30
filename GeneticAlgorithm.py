c4875247bb073968714aa52db3ad6b34b00dbc19
gioperca <76467920+gioperca@users.noreply.github.com>
2020-12-30 16:38:09 -0600

    add functión corsses and modific selection
    se agrego la función cruzamiento y se modifico la función selección para evitar que nos dieran los mismos, ahora nos dará el menor y el mayor

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -74,19 +74,75 @@ def costRow(matrix, Nind, columns, pob):
     return list
 
 
-def selectionFunction(pob,list, Nindi):
+"""
+pob es la una lista anidada donde se encuentra todos los posibles caminos
+list es la lista anidada que tiene los pesos de cada ciudad
+Nindi es el número de individuos en la población
+selOp variable que define que se quiere seleccionar si el mayor o menor
+"""
+def selectionFunction(pob,list, Nindi, selOp):
+    
+    if selOp == 0:
+        i=0
+        # Se toma el primer valor como el menor
+        menor= list[0]
+        for i in range(Nindi):
+            if menor > list[i]:
+                menor= list[i]
+            i+=1
+        i=0
+        for i in range(Nindi):
+            if menor == list[i]:
+                #se copia la lista que se encuentre en esa posición 
+                sel= pob[i]
+                break
+            i+=1
+    else:
+        i=0
+        # Se toma el primer valor como el mayor
+        mayor= list[0]
+        for i in range(Nindi):
+            if mayor < list[i]:
+                mayor= list[i]
+            i+=1
+        for i in range(Nindi):
+            if mayor == list[i]:
+                #se copia la lista que se encuentre en esa posición 
+                sel= pob[i]
+                break
+            i+=1
+    
+    return sel
+
+def corsses(sel, sel1, city):
     
     i=0
-    pos=0
-    menor= list[0]
+    pos1= random.randint(0, city/2)
+    #Se imprime solo para comprobar el valor
+    print(pos1)
+    pos2= random.randint(city/2, city-1)
+    print(pos2)
+    if pos1 == pos2:
+        pos1= random.randint(0, (city/2 - 1))
+        print(pos1)
+        pos2= random.randint(city/2 + 1, city-1)
+        print(pos2)
     
-    for i in range(Nindi):
-        if menor > list[i]:
-            menor= list[i]
-        i+=1
-    for i in range(Nindi):
-        if menor == list[i]:
-           break
-        i+=1
+    c1= sel[pos1+1:pos2]
+    c2= sel1[pos1+1:pos2]
+    #comprobar que numeros se van a copiar
+    print(c1)
+    print(c2)
     
-    return pob[i]
+    j= pos1
+    l= pos2-pos1
+    m=0
+    for j in range(l-1):
+        print(pos1)
+        sel[pos1]=c2[m]
+        sel1[pos1]=c1[m]
+        pos1+=1
+        m+=1
+        j+=1
+    
+    return[sel, sel1]
