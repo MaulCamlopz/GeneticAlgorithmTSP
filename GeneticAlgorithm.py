8102735b913fda6267d51ee44b72103c8f57cab0
Maul Camlopz <b22braul@live.com>
2021-01-02 22:48:32 -0600

    Add main and sumQualifications

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -9,104 +9,106 @@
 import math
 import random
 
-# Genera una población al azar
-def generatePopulation(nInd, longGenoma):
+# Genera una población de individuos aleatoriamente
+def generatePopulation(numInd, Genlength):
     population = []
-    for i in range(nInd):
+    for i in range(numInd):
         item = []
-        for j in range(longGenoma):
+        for j in range(Genlength):
             item.append(j+1)
         random.shuffle(item)
         population.append(item)
     return(population)
 
-# Intercambia las posiciones de un arreglo
-def swap(array, x, y):
-    temp = array[x] 
-    array[x] = array[y]
-    array[y] = temp
+# Intercambia dos posiciones de una lista
+def swap(list, x, y):
+    temp = list[x] 
+    list[x] = list[y]
+    list[y] = temp
 
-# Proceso de mutación por intercambio
-def mutation(array):
+# Proceso de mutación por intercambio para un individuo seleccionado
+def mutation(selectedInd):
     x = 0
     y = 0
     while x == y:
-        x = random.randrange(len(array))
-        y = random.randrange(len(array))
-    swap(array,x,y)
-    return array
+        x = random.randrange(len(selectedInd))
+        y = random.randrange(len(selectedInd))
+    swap(selectedInd,x,y)
+    return selectedInd
 
-# Crea la matriz con los pesos
-def createMatrix(rows, columns):
-    matrix = []
-    for i in range(rows):
-        column = []
-        for j in range(columns):
-            #Se modifico esta parte para que cuando sean entre ellos mismos sean cero ya que no hay
-            # una arista entre si misma
+# Crea una "matriz" con los pesos (distancias) entre cada vértice (ciudad)
+def createWeights(numInd):
+    weights = []
+    for i in range(numInd):
+        values = [] # lista de valores por cada individuo
+        for j in range(numInd):
             if i != j:
-                column.append(random.randint(10, 50))
+                values.append(random.randint(1, 100))
             else:
-                column.append(0)
-        matrix.append(column)
-    return(matrix)
+                values.append(0) # El peso es cero cuando nos referimos a la misma ciudad
+        weights.append(values)
+    return(weights)
 
-#Función de adaptacion
-#se corrigio la función ya esta probada y funciona
-def costRow(matrix, Nind, columns, pob):
+#Función de adaptación / evaluación
+def populationEvaluation(weights, numInd, genLength, pob):
     costo= 0
     list= []
-    i=0
-    j=0
     
-    for i in range(Nind):
+    for i in range(numInd):
         costo= 0
-        for j in range(columns-1):
+        for j in range(genLength-1):
             x= pob[i][j]
             y= pob[i][j+1]
-            costo= costo + matrix[x-1][y-1]
+            costo= costo + weights[x-1][y-1]
             j+=1
         primero= pob[i][0]
-        ultimo= pob[i][columns-1]
+        ultimo= pob[i][genLength-1]
         i+=1
-        list.append(costo+matrix[ultimo-1][primero-1])
+        list.append(costo+weights[ultimo-1][primero-1])
         
     return list
 
+# Función para sumar las calificaciones obtenidas por cada individuo
+def sumQualifications(qualifications):
+    result = 0
+    for q in qualifications:
+        result = result + q
+    return result
 
 """
-pob es la una lista anidada donde se encuentra todos los posibles caminos
-list es la lista anidada que tiene los pesos de cada ciudad
-Nindi es el número de individuos en la población
-selOp variable que define que se quiere seleccionar si el mayor o menor
+Función de selección
+    • pob: es la una lista anidada donde se encuentra todos los posibles caminos
+    • weights: es la lista anidada que tiene los pesos de cada ciudad
+    • numInd: es el número de individuos en la población
+    • selOp: variable que define que se quiere seleccionar si el mayor o menor
 """
-def selectionFunction(pob,list, Nindi, selOp):
+def selection(pob, weights, numInd, selOp):
     
     if selOp == 0:
-        i=0
         # Se toma el primer valor como el menor
-        menor= list[0]
-        for i in range(Nindi):
-            if menor > list[i]:
-                menor= list[i]
+        menor= weights[0]
+        
+        for i in range(numInd):
+            if menor > weights[i]:
+                menor= weights[i]
             i+=1
-        i=0
-        for i in range(Nindi):
-            if menor == list[i]:
+            
+        for i in range(numInd):
+            if menor == weights[i]:
                 #se copia la lista que se encuentre en esa posición 
                 sel= pob[i]
                 break
             i+=1
     else:
-        i=0
         # Se toma el primer valor como el mayor
-        mayor= list[0]
-        for i in range(Nindi):
-            if mayor < list[i]:
-                mayor= list[i]
+        mayor= weights[0]
+        for i in range(numInd):
+            if mayor < weights[i]:
+                mayor= weights[i]
             i+=1
-        for i in range(Nindi):
-            if mayor == list[i]:
+        
+        for i in range(numInd):
+            if mayor == weights[i]:
                 #se copia la lista que se encuentre en esa posición 
                 sel= pob[i]
                 break
@@ -114,7 +116,7 @@ def selectionFunction(pob,list, Nindi, selOp):
     
     return sel
 
-def corsses(sel, sel1, city):
+def crossover(sel, sel1, city):
     
     i=0
     pos1= random.randint(0, city/2)
@@ -146,3 +148,52 @@ def corsses(sel, sel1, city):
         j+=1
     
     return[sel, sel1]
+
+"""
+Función principal del AG
+    • numInd: Número de individuos de la población.
+    • genLength: longitud del Genoma.
+    • numGenerations: número de Generaciones.
+"""
+def main(numInd, genLength, numGenerations):
+    S = 0.0
+    weights = []
+    evaluation = []
+    population = []
+    newPopulation = [] 
+    selectedPob = [] # Individuos seleccionados
+    selOp = 0 # Se toma el primer valor como el menor
+    weights = createWeights(genLength)
+    population = generatePopulation(numInd, genLength)
+    print("Población original")
+    print(population)
+    
+    for i in range(numGenerations):
+        evaluation = populationEvaluation(weights, numInd, genLength, population)
+        print("Evaluación de la pob: ")
+        print(evaluation)
+        S = sumQualifications(evaluation)
+        print("Calificación:",S," \n")
+        print("---------------------")
+        
+        while len(newPopulation) < numInd:
+            selectedPob.append(selection(population, weights, numInd, selOp))
+            selectedPob.append(selection(population, weights, numInd, selOp))
+            print("Individuos seleccionados: ")
+            print(selectedPob)
+            selectedPob = crossover(selectedPob[0], selectedPob[1], genLength)
+            print("Cruza: ")
+            print(selectedPob)
+            mutation(selectedPob[0])
+            mutation(selectedPob[1])
+            print("Mutación")
+            print(selectedPob)
+            newPopulation = newPopulation + selectedPob
+            selectedPob = []
+    
+    evaluation = populationEvaluation(weights, numInd, genLength, population)
+    print("Evaluación final de la pob: ")
+    print(evaluation)
+    S = sumQualifications(evaluation)
+    print("Calificación final:",S," \n")
+    print("---------------------")
