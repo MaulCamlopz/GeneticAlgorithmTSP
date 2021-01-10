7c2f00c0d006d83d698161b1ac14fff57311e66d
Maul Camlopz <b22braul@live.com>
2021-01-10 02:10:32 -0600

    Update> generatePopulation, mutation, selection, main. Add> getMinList.

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -10,10 +10,15 @@ import math
 import random
 
 # Genera una población de individuos aleatoriamente
-def generatePopulation():
+def generatePopulation(numInd, genLength):
     population = []
-    population = [[1,2,3,4], [1,2,4,3], [1,3,2,4]]
-    
+    #population = [[1,2,3,4], [1,2,4,3], [1,3,2,4]]
+    for i in range(numInd):
+        item = []
+        for j in range(genLength):
+            item.append(j+1)
+        random.shuffle(item)
+        population.append(item)
     return(population)
 
 # Intercambia dos posiciones de una lista
@@ -23,20 +28,29 @@ def swap(list, x, y):
     list[y] = temp
 
 # Proceso de mutación por intercambio para un individuo seleccionado
-def mutation(selectedInd):
-    x = 0
-    y = 0
-    while x == y:
-        x = random.randrange(len(selectedInd))
-        y = random.randrange(len(selectedInd))
-    swap(selectedInd,x,y)
-    return selectedInd
+def mutation(selectedPob, pM):
+    i= 0
+    q = 0.0
+    while i < 2:
+        q = random.random()
+        if q < pM:
+            x = 0
+            y = 0
+            print("\nMutación\n")
+            print(selectedPob[i])
+            while x == y:
+                x = random.randrange(len(selectedPob[i]))
+                y = random.randrange(len(selectedPob[i]))
+            swap(selectedPob[i],x,y)
+            print(selectedPob[i])
+            print("\n")   
+        i+=1
+
 
 # Crea una "matriz" con los pesos (distancias) entre cada vértice (ciudad)
 def createWeights():
     weights = []
     weights = [[0, 7, 9, 8], [7, 0, 10, 4], [9, 10, 0, 15], [8, 4, 15, 0]]
-    
     return(weights)
 
 #Función de adaptación / evaluación
@@ -65,46 +79,33 @@ def sumQualifications(qualifications):
         result = result + q
     return result
 
+def getMinList(list):
+    minList = 0
+    value = list[0]
+    for i in range(len(list)):
+        if list[i] < value:
+            value = list[i]
+            minList = i
+    return minList
+
 """
-Función de selección
+Función de selección por torneo
     • pob: es la una lista anidada donde se encuentra todos los posibles caminos
     • weights: es la lista anidada que tiene los pesos de cada ciudad
-    • numInd: es el número de individuos en la población
+    • genLength: es el número de longitud del genoma (4)
     • selOp: variable que define que se quiere seleccionar si el mayor o menor
 """
-def selection(pob, weights, numInd, selOp):
-    
-    if selOp == 0:
-        # Se toma el primer valor como el menor
-        menor= weights[0]
-        
-        for i in range(numInd):
-            if menor > weights[i]:
-                menor= weights[i]
-            i+=1
-            
-        for i in range(numInd):
-            if menor == weights[i]:
-                #se copia la lista que se encuentre en esa posición 
-                sel= pob[i]
-                break
-            i+=1
-    else:
-        # Se toma el primer valor como el mayor
-        mayor= weights[0]
-        for i in range(numInd):
-            if mayor < weights[i]:
-                mayor= weights[i]
-            i+=1
-        
-        for i in range(numInd):
-            if mayor == weights[i]:
-                #se copia la lista que se encuentre en esa posición 
-                sel= pob[i]
-                break
-            i+=1
-    
-    return sel
+def selection(weights, numInd, genLength, pob):
+    random.shuffle(pob)
+    e = populationEvaluation(weights, numInd, genLength, pob)
+    value = e[0]
+    minList = 0
+    r = random.randint(2, 3)
+    for i in range(r):
+        if e[i] < value:
+            value = e[i]
+            minList = i
+    return pob[minList]
 
 #City es el número de ciudades
 def corsses(sel, sel1, city):
@@ -150,11 +151,13 @@ def corsses(sel, sel1, city):
 """
 Función principal del AG
     • numInd: Número de individuos de la población, en este ejemplo será de 3.
-    • genLength: longitud del Genoma, tiene una longitud de 4.
+    • GENOME_LENGTH: longitud del Genoma, tiene una longitud de 4.
     • numGenerations: número de Generaciones.
+    • pM: probabilidad de mutación
 """
-def main(numInd, genLength, numGenerations):
+def main(numInd, pM, numGenerations):
     S = 0.0
+    GENOME_LENGTH = 4
     weights = []
     evaluation = []
     population = []
@@ -163,30 +166,35 @@ def main(numInd, genLength, numGenerations):
     selMin = 0 # Se utiliza para que nos de el individuo con menos peso
     selMax = 1 #Se utiliza para que nos de el individuo con mayor peso
     weights = createWeights()
-    population = generatePopulation()
+    population = generatePopulation(numInd, GENOME_LENGTH)
     print("Población original")
     print(population)
     
     for i in range(numGenerations):
-        evaluation = populationEvaluation(weights, numInd, genLength, population)
+        evaluation = populationEvaluation(weights, numInd, GENOME_LENGTH, population)
         print("Evaluación de la pob: ")
         print(evaluation)
+        print("El mejor de la generación es el: ",getMinList(evaluation),"\n")
         S = sumQualifications(evaluation)
         print("Calificación:",S," \n")
         print("---------------------")
         
         while len(newPopulation) < numInd:
-            selectedPob.append(selection(population, weights, numInd, selMin))
-            selectedPob.append(selection(population, weights, numInd, selMax))
+
+            selectedPob.append(selection(weights, numInd, GENOME_LENGTH, population));
+            selectedPob.append(selection(weights, numInd, GENOME_LENGTH, population));
             print("Individuos seleccionados: ")
             print(selectedPob)
-            selectedPob = corsses(selectedPob[0], selectedPob[1], genLength)
+
+            selectedPob = corsses(selectedPob[0], selectedPob[1], GENOME_LENGTH)
             print("Cruza: ")
             print(selectedPob)
-            mutation(selectedPob[0])
-            mutation(selectedPob[1])
-            print("Mutación")
+            
+            mutation(selectedPob, pM)
+            
+            print("selectedPob")
             print(selectedPob)
+            
             newPopulation = newPopulation + selectedPob
             selectedPob = []
         print("Nueva población:", i,"\n")
@@ -195,9 +203,10 @@ def main(numInd, genLength, numGenerations):
         population = newPopulation
         newPopulation = []
     
-    evaluation = populationEvaluation(weights, numInd, genLength, population)
+    evaluation = populationEvaluation(weights, numInd, GENOME_LENGTH, population)
     print("Evaluación final de la pob: ")
     print(evaluation)
+    print("El mejor de la generación es el: ",getMinList(evaluation),"\n")
     S = sumQualifications(evaluation)
     print("Calificación final:",S," \n")
     print("---------------------")
